17a219d8d2502bb674e38a98c7145c3151e9f8c7
Philipp Fehre <philipp.fehre@googlemail.com>
2014-03-21 17:30:12 +0100

    fix view...

--- scripts/setup_views.js
+++ scripts/setup_views.js
@@ -6,8 +6,8 @@ var db = require("../lib/db")
 var countsDDoc = {
   "views": {
     "counts": {
-      "map": "function (doc, meta) { if(doc[0]) { for(var i = 0; i < doc.length; i++) { var qid = doc[i].id; var choices = doc[i].choices; for(var j = 0; j < choices.length; j++) { var cid = choices[j].id; emit([qid, cid], 1); } } } }",
-      "reduce": "_count"
+      "map": "function (doc, meta) { if(doc[0]) { for(var i = 0; i < doc.length; i++) { var qid = doc[i].id; var choices = doc[i].choices; for(var j = 0; j < choices.length; j++) { var choice = choices[j]; if(choice.state) { emit([qid, choice.id], 1); } else { emit([qid, choice.id], 0);} } } } }",
+      "reduce": "_sum"
     }
   }
 }
