423b1529122bd4b4d4088c3d9a2184452f1f0387
Philipp Fehre <philipp.fehre@googlemail.com>
2014-03-21 13:22:45 +0100

    handle cas in updates

--- test/question_list_test.js
+++ test/question_list_test.js
@@ -1,15 +1,15 @@
 var helper = require("./test_helper")
 var assert = require("assert")
-var fs = require("fs")
 var QuestionList = require("../lib/question_list")
 
 describe("QuestionList", function (){
   var list
-  before(function () {
-    var path = __dirname + "/../config/default_question_list.json"
-    var data = fs.readFileSync(path, {encoding: "utf8"})
-    list = JSON.parse(data).questions
-    list[0].choices[0].state = true
+  beforeEach(function (done) {
+    QuestionList.forUser(helper.user, function (err, loaded) {
+      list = loaded
+      list.questions[0].choices[0].state = true
+      done()
+    })
   })
 
   afterEach(function (done) {
@@ -32,6 +32,22 @@ describe("QuestionList", function (){
       QuestionList.forUser(helper.user, function (err, list) {
         assert(list.questions[0].choices[0].state)
         done()
+      })
+    })
+  })
+
+  it("disallows overwrite without cas", function (done) {
+    QuestionList.forUser(helper.user, function (err, list1) {
+      QuestionList.forUser(helper.user, function (err, list2) {
+        list1.questions[0].choices[0].state = true
+        list2.questions[0].choices[1].state = true
+        QuestionList.saveForUser(helper.user, list1, function (err) {
+          assert.ifError(err)
+          QuestionList.saveForUser(helper.user, list2, function (err) {
+            assert(err)
+            done()
+          })
+        })
       })
     })
   })
